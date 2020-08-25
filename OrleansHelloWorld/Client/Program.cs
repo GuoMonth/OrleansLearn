e54f7ad2623139f23d06a26c24db3aa13da437d2
guoshenga <guoshenga@digitalchina.com>
2020-08-25 18:54:54 +0800

    helloworld

--- OrleansHelloWorld/Client/Program.cs
+++ OrleansHelloWorld/Client/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Are you ready? (y/n)");
+            var enterStr = Console.ReadLine();
+            if (!"y".Equals(enterStr.Trim(), StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await ConnectClient())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nException while trying to run client: {ex.Message}");
+                Console.WriteLine("Make sure the silo the client is trying to connect to is running.");
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> ConnectClient()
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ClusterId = "OrleansBasics";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client successfully connected to silo host \n");
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            // example of calling grains from the initialized client
+            var friend = client.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Good moring,Hello Grain!");
+            Console.WriteLine($"\n\n{response}\n\n");
         }
     }
 }
